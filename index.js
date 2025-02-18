6426ada1f7296bf3166cbf77b55b35292f7269fa
jclevecodes <joshua.cleveland02@gmail.com>
2025-02-18 14:16:54 -0500

    handling of image data

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const AWS = require('aws-sdk')
 const s3 = new AWS.S3();
 const rekognition = new AWS.Rekognition();
+
+exports.handler = async (event) => {
+    //Function logic will be developed here
+}
+
+const body = JSON.parse(event.body);
+const imageData = body.imageData;
+
+const buffer = Buffer.from(imageData, 'base64')
+
+const bucketName = "jc-image-upload-bucket";
+const imageKey = `uploads/${Date.now()}.jpg`
+
+test
