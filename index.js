d237a0a6da0deb27c6a0c44870eaefcd786b26a8
jclevecodes <joshua.cleveland02@gmail.com>
2025-02-19 22:50:18 -0500

    object creation of bucket and rekognition

--- index.js
+++ index.js
@@ -14,4 +14,28 @@ const buffer = Buffer.from(imageData, 'base64')
 const bucketName = "jc-image-upload-bucket";
 const imageKey = `uploads/${Date.now()}.jpg`
 
-test
+await s3.putObject({
+    Bucket:  bucketNamem,
+    Key: imageKey,
+    Body: buffer,
+    ContentType: 'image/jpeg'
+}).promise();
+
+const rekognitionParams = {
+    Image: {
+        S3Object: {
+            Bucket: bucketName,
+            Name: imageKey
+        }
+    },
+    MaxLabels: 10,
+    MinConfidence: 70
+};
+
+const rekognitionResult = await rekognition.detectLabels(rekognitionParams).promise();
+
+return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(rekognitionResult)
+};
